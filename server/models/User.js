d16eedde9be3ec1d134c3c769e435c00e38ce0f0
tbohn2 <tanner.bohn@yahoo.com>
2023-11-02 18:23:45 -0700

    Started User model

---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: {
+            validator: function (value) {
+                return /\S+@\S+\.\S+/.test(value);
+            },
+            message: props => `${props.value} is not a valid email address!`,
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+});
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
