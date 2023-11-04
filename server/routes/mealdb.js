416e558d7b13f5289b62239e5884a646b4216668
tbohn2 <tanner.bohn@yahoo.com>
2023-11-03 22:29:03 -0700

    Created async functions for API fetches

--- server/routes/mealdb.js
+++ server/routes/mealdb.js
@@ -2,19 +2,48 @@ require("dotenv").config();
 
 const apiKey = process.env.API_KEY;
 
-const apiUrl = `https://www.themealdb.com/api/json/v1/${apiKey}/random.php`;
+const apiUrl = `https://www.themealdb.com/api/json/v1/${apiKey}/`;
 
-fetch(apiUrl)
-    .then(response => {
-        if (response.status === 200) {
-            return response.json();
-        } else {
-            console.error(`Error: ${response.status}`);
-        }
-    })
-    .then(data => {
+const fetchRandomMeal = async () => {
+    try {
+        const response = await fetch(`${apiUrl}random.php`);
+        const data = await response.json();
         console.log(data);
-    })
-    .catch(error => {
-        console.error('An error occurred:', error);
-    });
+        if (response.status !== 200) {
+            console.log(response);
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+};
+
+const fetchMealByName = async (name, category) => {
+    if (name && category) {
+        try {
+            const response = await fetch(`${apiUrl}search.php?s=${name}&filter.php?c=${category}`);
+            const data = await response.json();
+            console.log(data);
+            if (response.status !== 200) {
+                console.log(response);
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    if (name && !category) {
+        try {
+            const response = await fetch(`${apiUrl}search.php?s=${name}`);
+            const data = await response.json();
+            console.log(data);
+            if (response.status !== 200) {
+                console.log(response);
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+};
