c50ac818bdb01ea1bc517f38ee142593d0a9af0e
tbohn2 <tanner.bohn@yahoo.com>
2023-11-09 21:48:18 -0700

    Started resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,41 @@
+const { User, Recipe } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find();
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username });
+        },
+        recipes: async () => {
+            return Recipe.find();
+        },
+        recipe: async (parent, { recipeId }) => {
+            return Recipe.findOne({ _id: recipeId });
+        }
+    },
+    Mutation: {
+        addUser: async (parent, { name, email, password }) => {
+            const user = await User.create({ name, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        updateUserList: async (parent, { userId, shoppingList }) => {
+            return User.findOneAndUpdate({ _id: userId }, { shoppingList }, { new: true });
+        },
+    }
+};
