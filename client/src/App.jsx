30c5ae08ed5d21792d5e903e7c8e32db8e17fa50
tbohn2 <tanner.bohn@yahoo.com>
2023-11-21 18:00:49 -0700

    Added apollo provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,48 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Home, Login } from './pages';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './styles/root.css'
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <div className='myBody'>
-        <Header />
-        <div>
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/login' element={<Login />} />
-          </Routes>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className='myBody'>
+          <Header />
+          <div>
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/login' element={<Login />} />
+            </Routes>
+          </div>
+          <div>
+            <Footer />
+          </div>
         </div>
-        <div>
-          <Footer />
-        </div>
-      </div>
-    </Router>
+      </Router>
+    </ApolloProvider>
   );
 }
 
