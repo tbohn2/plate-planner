43250a870635fa28200af05e82628b2d5d7ad467
tbohn2 <tanner.bohn@yahoo.com>
2023-11-30 18:55:09 -0700

    Started query and mutations for recipes

--- client/src/pages/UserRecipes.jsx
+++ client/src/pages/UserRecipes.jsx
@@ -5,6 +5,32 @@ import { CREATE_RECIPE, SAVE_RECIPE_TO_USER, UPDATE_RECIPE } from '../utils/muta
 import Auth from '../utils/auth';
 
 const UserRecipes = () => {
+    if (!Auth.loggedIn()) {
+        window.location.assign('/login');
+    }
+
+    const user = Auth.getProfile();
+    const id = user.data._id;
+
+    const { loading, error, data } = useQuery(QUERY_SAVED_RECIPES, {
+        variables: { userId: id },
+    });
+
+    const recipes = data.savedRecipes || [];
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error! {error.message}</div>;
+    }
+
+    const [createRecipe] = useMutation(CREATE_RECIPE);
+    const [updateRecipe] = useMutation(UPDATE_RECIPE);
+    const [saveRecipeToUser] = useMutation(SAVE_RECIPE_TO_USER);
+
+
 
     return (
         <div className=''>
