40af4b125fe4f9cea7db0b125e9ba10137b9e1d6
tbohn2 <tanner.bohn@yahoo.com>
2023-12-02 20:34:38 -0700

    Added modal to create recipe

--- client/src/pages/UserRecipes.jsx
+++ client/src/pages/UserRecipes.jsx
@@ -12,6 +12,12 @@ const UserRecipes = () => {
     const user = Auth.getProfile();
     const id = user.data._id;
 
+    const [newRecipe, setNewRecipe] = useState({
+        title: '',
+        ingredients: '',
+        instructions: '',
+    });
+
     const { loading, error, data } = useQuery(QUERY_SAVED_RECIPES, {
         variables: { id: id },
     });
@@ -24,7 +30,6 @@ const UserRecipes = () => {
         return <div>Error! {error.message}</div>;
     }
 
-    console.log(data);
     const recipes = data.user.savedRecipes || [];
 
     const customRecipes = recipes.filter((recipe) => recipe.custom);
@@ -33,6 +38,21 @@ const UserRecipes = () => {
     // const [createRecipe] = useMutation(CREATE_RECIPE);
     // const [updateRecipe] = useMutation(UPDATE_RECIPE);
     // const [saveRecipeToUser] = useMutation(SAVE_RECIPE_TO_USER);
+
+
+
+    const handleRecipeChange = (e) => {
+        const { name, value } = e.target;
+        setNewRecipe((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const handleNewRecipe = (e) => {
+        e.preventDefault();
+        console.log(newRecipe);
+    };
 
 
 
@@ -47,6 +67,57 @@ const UserRecipes = () => {
                     </div>
                 ))}
             </div>
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Create New Recipe</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={handleNewRecipe}>
+                                <p>Recipe Title</p>
+                                <input
+                                    type="text"
+                                    name="title"
+                                    value={newRecipe.title}
+                                    onChange={handleRecipeChange}
+                                />
+                                <p>Ingredients</p>
+                                <input
+                                    type="text"
+                                    name="ingredients"
+                                    value={newRecipe.ingredients}
+                                    onChange={handleRecipeChange}
+                                />
+                                <p>Instructions</p>
+                                <input
+                                    type="text"
+                                    name="instructions"
+                                    value={newRecipe.instructions}
+                                    onChange={handleRecipeChange}
+                                />
+                                <p>Image</p>
+                                <input
+                                    type="text"
+                                    name="image"
+                                    value={newRecipe.image}
+                                    onChange={handleRecipeChange}
+                                />
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" className="btn btn-primary">Save Recipe</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <h1>Saved Recipes</h1>
             <div className=''>
                 {savedRecipes.map((recipe) => (
