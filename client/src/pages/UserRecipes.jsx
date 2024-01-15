5c7dcd661a4efb29ed4cb724878209c8d59436c0
tbohn2 <tanner.bohn@yahoo.com>
2024-01-15 12:25:05 -0700

    Grocery list displayed on UserRecipes page

--- client/src/pages/UserRecipes.jsx
+++ client/src/pages/UserRecipes.jsx
@@ -31,21 +31,45 @@ const UserRecipes = () => {
     const customRecipes = recipes.filter((recipe) => recipe.custom);
     const savedRecipes = recipes.filter((recipe) => !recipe.custom);
 
+    const shoppingList = data.user.shoppingList;
+
     return (
-        <div className=''>
-            <h1>My Recipes</h1>
-            <RecipeCards recipes={customRecipes} />
+        <div className='d-flex'>
+            <div className='col-8'>
+                <h1>My Recipes</h1>
+                <RecipeCards recipes={customRecipes} />
 
-            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#NewRecipeModal">
-                Create New Recipe
-            </button>
+                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#NewRecipeModal">
+                    Create New Recipe
+                </button>
 
-            <div className="modal fade" id="NewRecipeModal" tabIndex="-1" aria-labelledby="NewRecipeModalLabel" aria-hidden="true">
-                <NewRecipeForm />
+                <div className="modal fade" id="NewRecipeModal" tabIndex="-1" aria-labelledby="NewRecipeModalLabel" aria-hidden="true">
+                    <NewRecipeForm />
+                </div>
+
+                <h1>Saved Recipes</h1>
+                <RecipeCards recipes={savedRecipes} />
             </div>
 
-            <h1>Saved Recipes</h1>
-            <RecipeCards recipes={savedRecipes} />
+            <div className='col-4 border'>
+                <h1>My Shopping List</h1>
+                <div className='d-flex'>
+                    <h2 className='col-8'>Ingredients</h2>
+                    <h2 className='col-4'>Quantity</h2>
+                </div>
+                <div className='d-flex'>
+                    <ul className='list-unstyled col-8'>
+                        {shoppingList.map((ingredient) => (
+                            <li key={ingredient._id}>{ingredient.name}</li>
+                        ))}
+                    </ul>
+                    <ul className='list-unstyled col-4'>
+                        {shoppingList.map((ingredient) => (
+                            <li key={ingredient._id}>{ingredient.quantity}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
