6afb14cf0d0e407c29bc9bf80afb917042603791
tbohn2 <tanner.bohn@yahoo.com>
2024-01-18 12:42:32 -0700

    Edit form and state toggled by edit button

--- client/src/components/RecipeModal.jsx
+++ client/src/components/RecipeModal.jsx
@@ -6,9 +6,31 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
 
     const { _id, name, ingredients, URL, img } = recipe;
 
+    const [editing, setEditing] = useState(false);
+    const [editFormNameState, setEditFormNameState] = useState(name);
+    const [editFormIngedientsState, setEditFormIngredientsState] = useState(ingredients);
+
     const [addItemsToList] = useMutation(ADD_ITEMS_TO_LIST);
     const [updateRecipe] = useMutation(UPDATE_RECIPE);
     const [deleteRecipe] = useMutation(DELETE_RECIPE);
+
+    const toggleEdit = (e) => {
+        e.preventDefault();
+        setEditFormIngredientsState(ingredients);
+        setEditing(!editing);
+    };
+
+    const handleIngredientChange = (event, index) => {
+        const { name, value } = event.target;
+        setEditFormIngredientsState(prevIngredients => {
+            const updatedIngredients = [...prevIngredients];
+            updatedIngredients[index] = {
+                ...updatedIngredients[index],
+                [name]: name === 'quantity' ? Number(value) : value
+            };
+            return updatedIngredients;
+        });
+    };
 
     const addItemsToListHandler = async (event) => {
         event.preventDefault();
@@ -28,6 +50,8 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
             console.error(err);
         }
     }
+
+
 
     const removeRecipe = async (event) => {
         event.preventDefault();
@@ -50,17 +74,32 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
                     <h1 className="modal-title fs-5" id="exampleModalLabel">{name}</h1>
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div className="modal-body d-flex flex-column align-items-center">
-                    {ingredients.map((ingredient) => (
-                        <div key={ingredient._id} className="col-6 d-flex justify-content-between">
-                            <p>{ingredient.name}</p>
-                            <p>{ingredient.quantity}</p>
-                        </div>
-                    ))}
-                </div>
+                {editing ? (
+                    <div className="modal-body d-flex flex-column align-items-center">
+                        <form className="d-flex flex-column align-items-center">
+                            {editFormIngedientsState.map((ingredient, index) => (
+                                <div key={index} className="col-6 d-flex justify-content-between">
+                                    <input type="text" name="name" value={ingredient.name} onChange={(e) => handleIngredientChange(e, index)} />
+                                    <input type="number" name="quantity" value={ingredient.quantity} onChange={(e) => handleIngredientChange(e, index)} />
+                                </div>
+                            ))}
+                        </form>
+                        <button type="button" className="btn btn-primary" onClick={toggleEdit}>Save</button>
+                    </div>
+                ) : (
+                    <div className="modal-body d-flex flex-column align-items-center">
+                        {ingredients.map((ingredient) => (
+                            <div key={ingredient._id} className="col-6 d-flex justify-content-between">
+                                <p>{ingredient.name}</p>
+                                <p>{ingredient.quantity}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+
                 <div className="modal-footer">
                     <button type="button" className="btn btn-success" onClick={addItemsToListHandler}>Add to List</button>
-                    <button type="button" className="btn btn-primary">Edit Recipe</button>
+                    <button type="button" className="btn btn-primary" onClick={toggleEdit}>Edit Recipe</button>
                     <button type="button" className="btn btn-danger" onClick={removeRecipe} data-bs-dismiss="modal">Delete Recipe</button>
                     <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
