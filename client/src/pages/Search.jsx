f6172241dc4693d39bdbe4a908fe986a08d83dd5
tbohn2 <tanner.bohn@yahoo.com>
2024-01-22 12:39:26 -0700

    Created function to fetch random recipe

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 const Search = () => {
+
+    const fetchRandomRecipe = async () => {
+        try {
+            const res = await fetch('http://localhost:3001/api/random')
+            const data = await res.json();
+            console.log(data);
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div>
             <h1>Search Page</h1>
+            <button onClick={fetchRandomRecipe}>Random Recipe</button>
         </div>
     );
 };
