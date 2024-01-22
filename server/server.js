329cc1fa919b6b395d8ba73d4434015dcc202502
tbohn2 <tanner.bohn@yahoo.com>
2024-01-22 12:38:45 -0700

    Added CORS middleware to allow origin localhost port 3000

--- server/server.js
+++ server/server.js
@@ -17,6 +17,12 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(mealDBRoutes);
