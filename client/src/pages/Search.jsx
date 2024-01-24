457013855caa784f717db856003b45f738ec9ead
tbohn2 <tanner.bohn@yahoo.com>
2024-01-24 11:24:22 -0700

    Random recipe data organized and displayed

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
+
+    const [recipes, setRecipes] = useState([]);
 
     const fetchRandomRecipe = async () => {
         try {
             const res = await fetch('http://localhost:3001/api/random')
             const data = await res.json();
-            console.log(data);
+            setRecipes(data.meals);
 
         } catch (error) {
             console.log(error);
@@ -17,6 +19,35 @@ const Search = () => {
         <div>
             <h1>Search Page</h1>
             <button onClick={fetchRandomRecipe}>Random Recipe</button>
+            <div>
+                {recipes.map((recipe) => {
+                    const ingredientKeys = Object.keys(recipe).filter(key => key.startsWith('strIngredient')).map(key => recipe[key]);
+                    const measureKeys = Object.keys(recipe).filter(key => key.startsWith('strMeasure')).map(key => recipe[key]);
+                    let ingredientsArray = [];
+                    for (let i = 0; i < ingredientKeys.length; i++) {
+                        if (ingredientKeys[i] !== '') {
+                            const newIngredient = { name: ingredientKeys[i], amount: measureKeys[i] };
+                            ingredientsArray.push(newIngredient);
+                        }
+                    }
+
+                    return (
+                        <div key={recipe.idMeal}>
+                            <h1>{recipe.strMeal}</h1>
+                            <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+                            <h2>Ingredients</h2>
+                            <ul>
+                                {ingredientsArray.map((ingredient) => {
+                                    return (
+                                        <li key={ingredient.name}>{ingredient.name} - {ingredient.amount}</li>
+                                    );
+                                })}
+                            </ul>
+                            <p>{recipe.strInstructions}</p>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
