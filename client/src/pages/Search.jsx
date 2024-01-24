116fa00030edc4ebd7fb36093f5fce2a92592ee0
tbohn2 <tanner.bohn@yahoo.com>
2024-01-24 11:53:05 -0700

    Add function to save recipe to user

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { CREATE_RECIPE, SAVE_RECIPE_TO_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 const Search = () => {
 
+    const user = Auth.getProfile();
+    const id = user.data._id;
+
     const [recipes, setRecipes] = useState([]);
+
+    const [createRecipe] = useMutation(CREATE_RECIPE);
+    const [saveRecipeToUser] = useMutation(SAVE_RECIPE_TO_USER);
 
     const fetchRandomRecipe = async () => {
         try {
@@ -15,6 +24,35 @@ const Search = () => {
         }
     };
 
+
+    const handleSaveRecipe = async (e, name, ingredients, instructions, URL, img) => {
+        e.preventDefault();
+        try {
+            const { data } = await createRecipe({
+                variables: {
+                    name,
+                    ingredients,
+                    instructions,
+                    URL,
+                    img,
+                    custom: false,
+                },
+            });
+            const recipeId = data.createRecipe._id;
+            await saveRecipeToUser({
+                variables: {
+                    userId: id,
+                    recipeId: recipeId,
+                },
+            });
+            if (saveRecipeToUser) {
+                console.log('Recipe saved!');
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
         <div>
             <h1>Search Page</h1>
@@ -23,27 +61,34 @@ const Search = () => {
                 {recipes.map((recipe) => {
                     const ingredientKeys = Object.keys(recipe).filter(key => key.startsWith('strIngredient')).map(key => recipe[key]);
                     const measureKeys = Object.keys(recipe).filter(key => key.startsWith('strMeasure')).map(key => recipe[key]);
-                    let ingredientsArray = [];
+                    const name = recipe.strMeal;
+                    const instructions = recipe.strInstructions;
+                    const img = recipe.strMealThumb;
+                    const URL = recipe.strSource;
+
+                    let ingredients = [];
                     for (let i = 0; i < ingredientKeys.length; i++) {
                         if (ingredientKeys[i] !== '') {
                             const newIngredient = { name: ingredientKeys[i], amount: measureKeys[i] };
-                            ingredientsArray.push(newIngredient);
+                            ingredients.push(newIngredient);
                         }
                     }
 
                     return (
                         <div key={recipe.idMeal}>
-                            <h1>{recipe.strMeal}</h1>
-                            <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+                            <h1>{name}</h1>
+                            <a href={URL}>{URL}</a>
+                            <img src={img} alt={name} />
                             <h2>Ingredients</h2>
                             <ul>
-                                {ingredientsArray.map((ingredient) => {
+                                {ingredients.map((ingredient) => {
                                     return (
-                                        <li key={ingredient.name}>{ingredient.name} - {ingredient.amount}</li>
+                                        <li key={ingredient.name + ingredient.amount}>{ingredient.name} - {ingredient.amount}</li>
                                     );
                                 })}
                             </ul>
-                            <p>{recipe.strInstructions}</p>
+                            <p>{instructions}</p>
+                            <button onClick={(e) => handleSaveRecipe(e, name, ingredients, instructions, URL, img)}>Save Recipe</button>
                         </div>
                     );
                 })}
