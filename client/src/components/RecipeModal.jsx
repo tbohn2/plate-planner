eb49be879d511e8ae93d1aad20e4e6664161f6e3
tbohn2 <tanner.bohn@yahoo.com>
2024-02-01 22:16:51 -0700

    Can edit name and instructions of recipe; added styles

--- client/src/components/RecipeModal.jsx
+++ client/src/components/RecipeModal.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import { useQuery, useMutation } from '@apollo/client';
 import { ADD_ITEMS_TO_LIST, UPDATE_RECIPE, DELETE_RECIPE } from '../utils/mutations';
 import '../styles/root.css';
+import '../styles/recipeModal.css';
 
 const RecipeModal = ({ recipe, refetch, userId }) => {
 
-    const { _id, name, ingredients, URL, img } = recipe;
+    const { _id, name, ingredients, instructions, URL, img } = recipe;
 
     const typelessIngredients = ingredients.map(ingredient => {
         const quantity = ingredient.amount.replace(/[^0-9/.]/g, '');
@@ -20,6 +21,7 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
     const [editing, setEditing] = useState(false);
     const [addingToList, setAddingToList] = useState(false);
     const [editFormNameState, setEditFormNameState] = useState(name);
+    const [editFormInstructionsState, setEditFormInstructionsState] = useState(instructions);
     const [editFormIngedientsState, setEditFormIngredientsState] = useState(typelessIngredients);
     const [addingFormIngedientsState, setAddingFormIngredientsState] = useState(defaultItemsList);
 
@@ -29,7 +31,9 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
 
     const toggleEdit = (e) => {
         e.preventDefault();
+        setEditFormNameState(name);
         setEditFormIngredientsState(typelessIngredients);
+        setEditFormInstructionsState(instructions);
         setEditing(!editing);
     };
 
@@ -37,6 +41,10 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
         e.preventDefault();
         setAddingFormIngredientsState(defaultItemsList);
         setAddingToList(!addingToList);
+    };
+
+    const handleNameChange = (event) => {
+        setEditFormNameState(event.target.value);
     };
 
     const handleIngredientChange = (event, index) => {
@@ -82,6 +90,10 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
         }
     };
 
+    const handlerInstructionsChange = (event) => {
+        setEditFormInstructionsState(event.target.value);
+    };
+
     const addItemsToListHandler = async (event) => {
         event.preventDefault();
         try {
@@ -102,13 +114,14 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
         const updatedIngredients = editFormIngedientsState.map(ingredient => {
             return { name: ingredient.name, amount: `${ingredient.quantity} ${ingredient.unit}` }
         });
+        console.log(name, editFormNameState, updatedIngredients, editFormIngedientsState, _id)
         try {
             const { data } = await updateRecipe({
                 variables: {
                     recipeId: _id,
                     name: editFormNameState,
                     ingredients: updatedIngredients,
-                    instructions: "",
+                    instructions: editFormInstructionsState,
                 },
             });
             if (data) {
@@ -136,17 +149,17 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
     return (
         <div className="modal-dialog modal-lg ">
             <div className="modal-content bg-light-yellow">
-                <div className="modal-header">
-                    <h1 className="modal-title">{name}</h1>
-                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => { setEditing(false); setAddingToList(false) }}></button>
-                </div>
                 {editing || addingToList ? (
                     <div>
                         {editing ? (
                             <div>
+                                <div className="modal-header border-bottom border-dark">
+                                    <input type="text" className="fs-2" name="name" value={editFormNameState} onChange={(e) => handleNameChange(e)} />
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => { setEditing(false); setAddingToList(false) }}></button>
+                                </div>
                                 <div className="modal-body d-flex flex-column align-items-center">
                                     <form className="d-flex flex-column align-items-center">
-                                        <h2 className="col-12 text-center border-bottom border-dark">Edit Recipe</h2>
+                                        <h2 className="col-12 text-center">Edit Recipe:</h2>
                                         <div className="col-10 d-flex">
                                             <h3 className="col-4 text-center text-decoration-underline">Ingredients</h3>
                                             <h3 className="col-2 text-center text-decoration-underline">Qty.</h3>
@@ -161,6 +174,10 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
                                             </div>
                                         ))}
                                         <button type='button' className="btn btn-success my-1" onClick={increaseIngredientNumber}>+ Ingredient</button>
+                                        <div className="col-12">
+                                            <h3 className="col-12 text-center text-decoration-underline">Instructions</h3>
+                                            <textarea className="col-12 form-control my-text-area" name="instructions" value={editFormInstructionsState} onChange={handlerInstructionsChange} />
+                                        </div>
                                     </form>
                                 </div>
                                 <div className="modal-footer">
@@ -170,9 +187,13 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
                             </div>
                         ) : (
                             <div>
+                                <div className="modal-header border-bottom border-dark">
+                                    <h1 className="modal-title">{name}</h1>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => { setEditing(false); setAddingToList(false) }}></button>
+                                </div>
                                 <div className="modal-body d-flex flex-column align-items-center">
                                     <form className="d-flex col-12 flex-column align-items-center">
-                                        <h2 className="col-12 text-center border-bottom border-dark">Add Ingredients to Grocery List</h2>
+                                        <h2 className="col-12 text-center">Add Ingredients to Grocery List:</h2>
                                         <div className="col-9 d-flex">
                                             <h3 className="col-6 text-center text-decoration-underline">Ingredients</h3>
                                             <h3 className="col-2 text-center text-decoration-underline">Qty.</h3>
@@ -197,29 +218,35 @@ const RecipeModal = ({ recipe, refetch, userId }) => {
                     </div>
                 ) : (
                     <div>
-                        <div className="modal-body d-flex justify-content-center col-12">
-                            <div className="col-5">
+                        <div className="modal-header border-bottom border-dark">
+                            <h1 className="modal-title">{name}</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => { setEditing(false); setAddingToList(false) }}></button>
+                        </div>
+                        <div className="modal-body d-flex col-12 justify-content-center">
+                            <div className="col-6 d-flex flex-column align-items-center">
                                 <h2 className="col-12 text-center text-decoration-underline">Ingredients</h2>
                                 {ingredients.map((ingredient) => (
-                                    <div key={ingredient._id} className="d-flex justify-content-between border-dark border">
+                                    <div key={ingredient._id} className="col-9 d-flex justify-content-between border-dark border">
                                         <p className="m-1">{ingredient.name}</p>
                                         <p className="m-1">{ingredient.amount}</p>
                                     </div>
                                 ))}
                             </div>
-                            {recipe.instructions || recipe.img ? (
-                                <div className="d-flex flex-column align-items-center">
-                                    {recipe.img ? (<img className="searchImg m-2" src={recipe.img} alt={recipe.name} />) : (null)}
-                                    {recipe.instructions ? (<div className="m-2">
-                                        <h2 className="col-12 text-center text-decoration-underline">Instructions</h2>
-                                        <p>{recipe.instructions}</p>
-                                    </div>) : (null)}
+                            {instructions || img ? (
+                                <div className=" col-6 d-flex flex-column align-items-center">
+                                    {img ? (<img className="searchImg m-2" src={img} alt={name} />) : (null)}
+                                    {instructions ? (
+                                        <div className="mx-2">
+                                            <h2 className="col-12 text-center text-decoration-underline">Instructions</h2>
+                                            <p>{instructions}</p>
+                                        </div>
+                                    ) : (null)}
                                 </div>
                             ) : (null)}
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-green" onClick={toggleAddingToList}>Add to List</button>
-                            <button type="button" className="btn btn-blue" onClick={toggleEdit}>Edit Recipe</button>
+                            <button type="button" className="btn btn-success" onClick={toggleAddingToList}>Add to List</button>
+                            <button type="button" className="btn btn-primary" onClick={toggleEdit}>Edit Recipe</button>
                             <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={removeRecipe}>Delete Recipe</button>
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         </div>
