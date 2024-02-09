020233f57bd7daabbe350af563f8cf3b84c62c56
tbohn2 <tanner.bohn@yahoo.com>
2024-02-09 11:31:47 -0700

    Made header responsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,18 +24,35 @@ const Header = () => {
         <div>
             {isMobile ?
                 (
-                    <header></header>
+                    <header className='d-flex justify-content-center border-bottom border-dark'>
+                        <img src={logo1} alt='logo1' className='header-logo' />
+                        <div className="btn-group position-absolute top-0 end-0">
+                            <button type="button" className="bg-orange mobile-nav-btn z-0 noTextDec fs-1 border border-light text-light d-flex justify-content-center align-items-center" data-bs-toggle="dropdown" aria-expanded="false">
+                                ☰
+                            </button>
+                            <ul className="dropdown-menu bg-light-yellow">
+                                <li><Link className='text-decoration-none fs-3 dropdown-item' to='/myRecipes'>My Recipes</Link></li>
+                                <li><Link className='text-decoration-none fs-3 dropdown-item border-top border-dark' to='/list'>My List</Link></li>
+                                <li><Link className='text-decoration-none fs-3 dropdown-item border-top border-dark' to='/search'>Browse Recipes</Link></li>
+                                {loggedIn ? (
+                                    <li><Link className='text-decoration-none fs-3 dropdown-item border-top border-dark' onClick={Auth.logout}>Logout</Link></li>
+                                ) : (
+                                    <li><Link className='text-decoration-none fs-3 dropdown-item border-top border-dark' to='/login'>Login</Link></li>
+                                )}
+                            </ul>
+                        </div>
+                    </header>
                 )
                 :
                 (
                     <header className='d-flex justify-content-between border-bottom border-dark'>
-                        <img src={logo1} alt='logo1' className='ms-5' />
-                        <nav className='d-flex align-items-center justify-content-evenly col-6'>
-                            <Link className='text-decoration-none navBtn' to='/myRecipes'>My Recipes</Link>
-                            <Link className='text-decoration-none navBtn' to='/list'>My List</Link>
-                            <Link className='text-decoration-none navBtn' to='/search'>Browse Recipes</Link>
+                        <img src={logo1} alt='logo1' className='header-logo' />
+                        <nav className='d-flex align-items-center justify-content-evenly col-xxl-6 col-xl-7 col-lg-8 col-9'>
+                            <Link className='text-decoration-none navBtn px-1' to='/myRecipes'>My Recipes</Link>
+                            <Link className='text-decoration-none navBtn px-1' to='/list'>My List</Link>
+                            <Link className='text-decoration-none navBtn px-1' to='/search'>Browse Recipes</Link>
                             {loggedIn ? (
-                                <Link className='text-decoration-none loginBtn' onClick={Auth.logout}>Logout</Link>
+                                <Link className='text-decoration-none loginBtn px-1' onClick={Auth.logout}>Logout</Link>
                             ) : (
                                 <Link className='text-decoration-none loginBtn' to='/login'>Login</Link>
                             )}
