388000bec64c58ff10582c775e6656be2af0af18
tbohn2 <tanner.bohn@yahoo.com>
2024-02-09 10:21:44 -0700

    Home page redirects to login page for now

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import '../styles/home.css'
+import auth from "../utils/auth";
 
 const Home = () => {
+    if (!auth.loggedIn()) {
+        window.location.assign('/login');
+    }
 
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
