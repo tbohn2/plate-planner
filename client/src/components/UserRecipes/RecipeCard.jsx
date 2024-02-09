372212f15f21d9b00707c70f548ac22eb1a4141c
tbohn2 <tanner.bohn@yahoo.com>
2024-02-09 09:02:10 -0700

    Added styles to recipe card

--- client/src/components/UserRecipes/RecipeCard.jsx
+++ client/src/components/UserRecipes/RecipeCard.jsx
@@ -8,10 +8,10 @@ const RecipeCard = ({ recipe, refetch, userId }) => {
     const [loaded, setLoaded] = useState(false);
 
     return (
-        <div className={`card recipeCard border-1 border-secondary-emphasis col-3 mx-4 my-2 ${recipe.img ? loaded ? 'fade-in' : 'visually-hidden' : 'fade-in'}`}>
-            <div key={recipe._id} className='d-flex flex-column align-items-center' data-bs-toggle="modal" data-bs-target={`#RecipeModal-${recipe._id}`}>
-                {recipe.img ? (<img className="img col-12" onLoad={() => setLoaded(true)} src={recipe.img} alt={recipe.name} />) : (null)}
-                <h3 className="text-center my-2 fw-bold">{recipe.name}</h3>
+        <div className={`card recipeCard border-1 border-secondary-emphasis col-3 mx-4 pb-1 my-2 ${recipe.img ? loaded ? 'fade-in' : 'visually-hidden' : 'fade-in'}`}>
+            <div key={recipe._id} className='d-flex flex-column align-items-center justify-content-between' data-bs-toggle="modal" data-bs-target={`#RecipeModal-${recipe._id}`}>
+                {recipe.img ? (<img className="img recipeImg col-12" onLoad={() => setLoaded(true)} src={recipe.img} alt={recipe.name} />) : (null)}
+                <h3 className="col-12 text-center my-2 fw-bold">{recipe.name}</h3>
             </div>
 
             <div className="modal fade" id={`RecipeModal-${recipe._id}`} tabIndex="-1" aria-labelledby={`RecipeModalLabel-${recipe._id}`} aria-hidden="true">
