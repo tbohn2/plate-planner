360097a14e705ded1a40f6dab3176edf815dee2f
tbohn2 <tanner.bohn@yahoo.com>
2024-02-12 14:42:41 -0700

    Window assigned to proper URL

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -26,7 +26,7 @@ class AuthService {
 
     login(idToken) {
         localStorage.setItem('id_token', idToken);
-        window.location.assign('/myRecipes');
+        window.location.assign('/plate-planner/myRecipes');
     }
 
     logout() {
