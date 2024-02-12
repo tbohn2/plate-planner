f669ff125b232d0e724cbc6428dd567bcda366e9
tbohn2 <tanner.bohn@yahoo.com>
2024-02-12 15:03:17 -0700

    Added deployed site as allowed origin

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ const server = new ApolloServer({
 });
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://tbohn2.github.io/plate-planner");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
