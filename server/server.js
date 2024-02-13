604f3049e4f747afb181ff4c65494161b8ab8f30
tbohn2 <tanner.bohn@yahoo.com>
2024-02-12 18:19:29 -0700

    Removed forward slash from URL

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const server = new ApolloServer({
 
 app.use(function (req, res, next) {
   // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // For development
-  res.header("Access-Control-Allow-Origin", "https://tbohn2.github.io/"); // For production
+  res.header("Access-Control-Allow-Origin", "https://tbohn2.github.io"); // For production
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
