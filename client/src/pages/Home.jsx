e4fe6499dda3a0db139635d94133290ba503ac93
tbohn2 <tanner.bohn@yahoo.com>
2024-02-19 11:18:13 -0700

    Added about and image section and styled

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,17 @@ import '../styles/root.css'
 import '../styles/home.css'
 import auth from "../utils/auth";
 import circleImg from '../assets/circleHomePage.png'
+import homeImg1 from '../assets/home1.png'
+import homeImg2 from '../assets/home2.png'
+import homeImg3 from '../assets/home3.png'
 
 const Home = () => {
 
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+    const cardInfo = [
+        { image: homeImg1, title: 'Step 1', text: 'Sign up and log in to your account' },
+        { image: homeImg2, title: 'Step 2', text: 'Search for your favorite recipes' },
+        { image: homeImg3, title: 'Step 3', text: 'Add ingredients to your shopping list' }]
 
     useEffect(() => {
         function handleResize() {
@@ -30,14 +37,30 @@ const Home = () => {
             {isMobile ? (
                 <div></div>
             ) : (
-                <div className="bg-blue">
-                    <div className="home-body-bg overflow-hidden"></div>
-                    <div>
+                <div className="bg-blue pb-3">
+                    <div className="home-body-bg">
                         <img src={circleImg} alt='circleImg' className='circleImg' />
-                        <div className="about">About</div>
+                        <div className="about text-dark d-flex flex-column align-items-center fs-1 p-1">
+                            <p className="bubblegum">Simplify your life by storing your favorite recipes and shopping list in the same place! This app allows you to create your
+                                shopping list at the click of a button! Join now for free to see it in action!</p>
+                            <button className="btn btn-success fs-4 col-8">Sign Up!</button>
+                        </div>
                     </div>
-                    <div className="bg-blue">
-                        <h1 className="text-light fw-bold col-12 text-center">How It Twerks</h1>
+                    <div className="howItWorks bg-blue d-flex flex-column align-items-center">
+                        <h1 className="text-light bubblegum fw-bold col-12 text-center my-5">HOW IT WORKS</h1>
+                        <div className="d-flex justify-content-center col-12">
+                            {cardInfo.map((card, index) => (
+                                <div key={index} className="card border-0 col-3 mx-5">
+                                    <img src={card.image} className="card-img-top" alt="..." />
+                                    <div className="card-body bg-blue text-light">
+                                        <h3 className="card-title bubblegum">{card.title}</h3>
+                                        <p className="card-text fs-5">{card.text}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <h2 className="text-light bubblegum text-center my-3">Sign up for free today!</h2>
+                        <button className="btn btn-success fs-3 col-3 text-center">Create Account</button>
                     </div>
                 </div>
             )}
